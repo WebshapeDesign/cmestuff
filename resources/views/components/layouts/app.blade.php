9df182f6b04e9977eb6464a6d42f7cdae878b656
Webshape Design <hello@webshapedesign.co.uk>
2025-04-21 10:50:30 +0100

    Initial Commit
    Upload

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+  <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+    @fluxAppearance
+</head>
+<body>
+    <h1>Hello!</h1>
+<x-layouts.app.sidebar :title="$title ?? null">
+    <flux:main>
+        {{ $slot }}
+    </flux:main>
+</x-layouts.app.sidebar>
+@fluxScripts
+</body>
+</html>
