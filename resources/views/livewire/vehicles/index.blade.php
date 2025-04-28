da9e3f9612ebb95389d84c07486ba82fe13aa799
Webshape Design <hello@webshapedesign.co.uk>
2025-04-28 08:13:17 +0100

    Breadcrumbs

--- resources/views/livewire/vehicles/index.blade.php
+++ resources/views/livewire/vehicles/index.blade.php
@@ -2,36 +2,13 @@
 <flux:main container>
         <div class="flex justify-between items-center mb-6">
             <div class="flex items-center gap-2">
-                <div class="flex items-center gap-2">
-                    <flux:select size="sm" class="">
-                        <option>Last 7 days</option>
-                        <option>Last 14 days</option>
-                        <option selected>Last 30 days</option>
-                        <option>Last 60 days</option>
-                        <option>Last 90 days</option>
-                    </flux:select>
+            <flux:breadcrumbs>
+    <flux:breadcrumbs.item href="#">Dasboard</flux:breadcrumbs.item>
+    <flux:breadcrumbs.item href="#">Vehicles</flux:breadcrumbs.item>
+</flux:breadcrumbs>
 
-                    <flux:subheading class="max-md:hidden whitespace-nowrap">compared to</flux:subheading>
+               
 
-                    <flux:select size="sm" class="max-md:hidden">
-                        <option selected>Previous period</option>
-                        <option>Same period last year</option>
-                        <option>Last month</option>
-                        <option>Last quarter</option>
-                        <option>Last 6 months</option>
-                        <option>Last 12 months</option>
-                    </flux:select>
-                </div>
-
-                <flux:separator vertical class="max-lg:hidden mx-2 my-2" />
-
-                <div class="max-lg:hidden flex justify-start items-center gap-2">
-                    <flux:subheading class="whitespace-nowrap">Filter by:</flux:subheading>
-
-                    <flux:badge as="button" variant="pill" color="zinc" icon="plus" size="lg">Amount</flux:badge>
-                    <flux:badge as="button" variant="pill" color="zinc" icon="plus" size="lg" class="max-md:hidden">Status</flux:badge>
-                    <flux:badge as="button" variant="pill" color="zinc" icon="plus" size="lg">More filters...</flux:badge>
-                </div>
             </div>
 
             <flux:tabs variant="segmented" class="w-auto! ml-2" size="sm">
