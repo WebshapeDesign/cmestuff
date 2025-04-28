ef4cc8eec5ed3baf62f1c521145d4d95901ff698
Webshape Design <hello@webshapedesign.co.uk>
2025-04-28 11:54:25 +0100

    fix2

--- app/Livewire/MileageLogs/Index.php
+++ app/Livewire/MileageLogs/Index.php
@@ -7,10 +7,25 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $sortBy = 'date';
+    public $sortDirection = 'desc';
+
+    public function sort($field)
+    {
+        if ($this->sortBy === $field) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortBy = $field;
+            $this->sortDirection = 'asc';
+        }
+    }
+
     public function render()
     {
         return view('livewire.mileage-logs.index', [
-            'mileageLogs' => MileageLog::with('vehicle', 'user')->latest()->paginate(10),
+            'mileageLogs' => MileageLog::with('vehicle', 'user')
+                ->orderBy($this->sortBy, $this->sortDirection)
+                ->paginate(10),
         ]);
     }
 }
