f7317d2f5254a119fe8708b15445c2916aa93858
Webshape Design <hello@webshapedesign.co.uk>
2025-04-28 12:20:56 +0100

    h

--- app/Livewire/Timesheets/Index.php
+++ app/Livewire/Timesheets/Index.php
@@ -10,10 +10,25 @@ class Index extends Component
 {
     use WithPagination;
 
+    public $sortBy = 'week_commencing';
+    public $sortDirection = 'desc';
+
+    public function sort($field)
+    {
+        if ($this->sortBy === $field) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortBy = $field;
+            $this->sortDirection = 'asc';
+        }
+    }
+
     public function render()
     {
         return view('livewire.timesheets.index', [
-            'timesheets' => Timesheet::where('user_id', auth()->id())->latest()->paginate(10),
+            'timesheets' => Timesheet::where('user_id', auth()->id())
+                ->orderBy($this->sortBy, $this->sortDirection)
+                ->paginate(10),
         ]);
     }
 }
